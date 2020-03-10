57cf8e4f13702f7b452a1f1f087e30039f33b8fe
alxogm <alxogm@github.com>
2020-03-09 22:43:13 -0700

    - Reading the BAL templates file to complete the catalog.         - Modified the BAL catolog saved to include BI_CIV and ERR_BI_CIV, just as an example...         - It works for current version of BAL_META but it is likely this           will change in quickquasars...

--- get_dla_bal_cat.py
+++ get_dla_bal_cat.py
@@ -1,19 +1,34 @@
+import os
 import glob as glob
 import numpy as np
 import argparse
 import fitsio
-from astropy.table import Table,vstack,join
+from astropy.table import Table,vstack,join,hstack
 import astropy.io.fits as pyfits
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--outdir',type=str,help='output directory of the quickquasar run')
 parser.add_argument('--idir',type=str,help='directory from where to fetch the input data')
 parser.add_argument('--catalog',type=str,default=None)
+
+
 args = parser.parse_args()
 
+## Check if BAL and DLA catalogs already exist
+bal_catfile=args.outdir+'/BAL_cat.fits'
+dla_catfile=args.outdir+'/DLA_cat.fits'
+
+
+    
+if (os.path.isfile(bal_catfile)) or (os.path.isfile(dla_catfile)):
+    print("catalogs already exist")
+    exit()
+    
 parent_dirs = glob.glob(args.idir+'/*')
 DLA = []
 BAL = []
+
 for dirs in parent_dirs:
     sub_dirs = glob.glob(dirs+'/*')
 
@@ -41,9 +56,14 @@ for dirs in parent_dirs:
             BAL = None
 
 if BAL is not None:
-    print('we are here')
-    BAL = np.hstack(BAL)
-    fitsio.write(args.outdir+'/BAL_cat.fits',BAL)
+    template_meta=fitsio.read('/global/cfs/cdirs/desi/spectro/templates/basis_templates/v3.2/bal_templates_v3.0.fits',columns=['BI_CIV','ERR_BI_CIV'])
+    BAL = Table(np.hstack(BAL))
+    index=BAL['TEMPLATEID']
+    template_meta=Table(template_meta[index])
+    BAL=hstack([BAL,template_meta])
+    BAL.write(bal_catfile)
+    #fitsio.write(bal_catfile,BAL)
+    
 if DLA is not None:
     DLA = np.hstack(DLA)
-    fitsio.write(args.outdir+'/DLA_cat.fits',DLA)
+    fitsio.write(dla_cat,DLA)
